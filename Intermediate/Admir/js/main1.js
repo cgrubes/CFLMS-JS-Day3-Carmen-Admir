9e4af70fbc344fd5cbc73cce4eb5bbe528de7e7e
Admir Saraseli <asaraseli@gmail.com>
2020-05-27 13:43:40 +0200

    add intermediate exercises

---
+++ Intermediate/Admir/js/main1.js
@@ -0,0 +1,6 @@
+let capital = (function(data) {
+  return data.charAt(0).toUpperCase() + data.slice(1);
+});
+
+document.write(capital("i am a web developer"))
+    
---
+++ Intermediate/Admir/js/main2.js
@@ -0,0 +1,11 @@
+function getGrades() {
+	let x = document.getElementById("grade").value; // This is the string
+	let data = x.split(", "); // This is the new array
+	let sum = 0;
+	for (i = 0; i < data.length; i++) {
+		sum += parseInt(data[i])  // This is sum of array
+	}
+	let avg = sum/data.length;
+	document.getElementById("demo").innerHTML = "Sum: " +sum+ "<br> Average: " +avg; 
+}
+
